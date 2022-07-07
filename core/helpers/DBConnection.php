0132ca8c2146ce748a6a57b8e6c2b47e1ee53bde
SerzhKorochan <serzhkorochan@gmail.com>
2022-07-07 09:11:03 +0300

    added db connection class

--- core/helpers/DBConnection.php
+++ core/helpers/DBConnection.php
@@ -2,12 +2,39 @@
 
     namespace Core\Helpers;
 
+    use \PDOException;
+    use \PDO;
+
+
     class DBConnection {
+        private $pdo;
         private $pathToConfigFile = "core/config/db.ini";
 
         function __construct() {
             $config = parse_ini_file($this->pathToConfigFile);
 
-            var_dump($config);
+            $dsnPattern = "mysql:host=%s;dbname=%s;charset=%s";
+            $dsn = sprintf(
+                $dsnPattern, 
+                $config['host'], 
+                $config['dbname'], 
+                $config['charset']
+            );
+
+            try {
+                $pdo = new PDO($dsn, $config['user'], $config['pass']);
+                $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+                echo "Connection okay.";
+                $this->pdo = $pdo;
+
+            } catch (PDOException $e) {
+                echo "Connection failed: ". $e->getMessage();
+            }
+
+        }
+
+        public function getPdoObj() {
+            return $this->pdo;
         }
     }
