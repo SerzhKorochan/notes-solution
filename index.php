2eee2e7af67e2c6d2042e132143dcaddb77bd0d8
SerzhKorochan <serzhkorochan@gmail.com>
2022-07-07 10:16:59 +0300

    added user model

--- index.php
+++ index.php
@@ -1,7 +1,19 @@
 <?php 
     require 'core/helpers/DBConnection.php';
+    require 'core/models/UserModel.php';
 
     use Core\Helpers\DBConnection;
+    use Core\Models\UserModel;
+
 
     $obj = new DBConnection;
+
+    $user = new UserModel(
+        $obj->getPdoObj(),
+        "serzhkorochan@gmail.commm1234",
+        "admin123"
+    );
+
+    // var_dump($user->isUserExist());
+    var_dump($user->createUser());
 ?>
